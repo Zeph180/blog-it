b9e4efa8affe39aae097bd7d975edf8bb35b9691
Zeph180 <zephrichards1@gmail.com>
2023-05-09 23:41:09 +0300

    Add refs

--- db/migrate/20230509094503_create_users.rb
+++ db/migrate/20230509094503_create_users.rb
@@ -5,9 +5,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, null: false, limit: 50
-      t.string :email, null: false, limit: 50
-      t.string :password_digest, null: false
-      t.string :photo_url
+      t.string :photo
+      t.string :bio
+      t.integer :posts_count, default: 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_103008) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_190716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title", limit: 50, null: false
-    t.text "body", null: false
-    t.bigint "user_id", null: false
+  create_table "comments", force: :cascade do |t|
+    t.string "text", limit: 1000, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", limit: 50, null: false
+    t.text "text", null: false
+    t.integer "likes_count", default: 0
+    t.integer "comments_count", default: 0
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 50, null: false
-    t.string "email", limit: 50, null: false
-    t.string "password_digest", null: false
-    t.string "photo_url"
+    t.string "photo"
+    t.string "bio"
+    t.integer "posts_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "users"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
 end
