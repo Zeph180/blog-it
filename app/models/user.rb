f394c6456654f6f18fbef513eed07a7597016032
Zeph180 <zephrichards1@gmail.com>
2023-05-11 12:03:58 +0300

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id'
+
+  def last_three_posts
+    self.posts.last(3)
+  end
+end
