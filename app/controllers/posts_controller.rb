dbbdff0c328d2c91d5f01e159d6b8213c972a917
Zeph180 <zephrichards1@gmail.com>
2023-05-15 15:10:46 +0300

    Add index on post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find_by(id: params[:id])
+    @posts = Post.where(user_id: params[:id]).order(created_at: :desc)
+  end
+  
   def show
     @post = Post.find_by(id: params[:id])
   end
