4de949b9080b87f1e40c8200bceab46edb0712ed
Zeph180 <zephrichards1@gmail.com>
2023-05-15 15:18:31 +0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @user = User.find_by(id: params[:id])
     @posts = Post.where(user_id: params[:id]).order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find_by(id: params[:id])
   end
