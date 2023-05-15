cc06f091a4b478d27e4b1d019bbd92072a79916e
Zeph180 <zephrichards1@gmail.com>
2023-05-15 13:48:16 +0300

    Add controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @posts = Post.where(user_id: params[:id]).order(created_at: :desc).limit(3)
+  end
+
+  def posts
+    @user = User.find_by(id: params[:id])
+    @posts = Post.where(user_id: params[:id]).order(created_at: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/posts', to: 'users#posts', as: 'user_posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
   # Defines the root path route ("/")
   # root "articles#index"
