ac17f10f4d2fb6715addef745d5877e10f41f286
Zeph180 <zephrichards1@gmail.com>
2023-05-15 14:33:01 +0300

    Add post index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
   get 'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id/posts', to: 'users#posts', as: 'user_posts'
+  get 'users/:id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
   # Defines the root path route ("/")
