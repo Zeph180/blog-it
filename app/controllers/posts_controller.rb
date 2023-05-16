02499de0ee830fd3d5b22bdbed2858399d1a9f7b
Zeph180 <zephrichards1@gmail.com>
2023-05-16 13:27:06 +0300

    Improve possts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:id])
-    @posts = Post.where(user_id: params[:id]).order(created_at: :desc)
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
   end
 end
