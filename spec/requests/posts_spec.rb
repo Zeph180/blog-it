0b7af07e1d0ec404b19a36cbdfef094b97ac278b
Zeph180 <zephrichards1@gmail.com>
2023-05-16 13:28:40 +0300

    Create posrs spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,41 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :request do
-  describe "GET index" do
-    let!(:user) { User.create(name: "John", photo: "john@example.com", bio: "Il love forever") }
-    let!(:posts) do
-      [
-        Post.create(title: "First Post", author_id: user),
-        Post.create(title: "Second Post", author_id: user),
-        Post.create(title: "Third Post", author_id: user)
-      ]
-    end
+RSpec.describe PostsController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns the user and posts' do
+      user = User.create(name: 'John Doe')
+      post1 = Post.create(title: 'First Post', text: 'Lorem ipsum', author_id: user.id)
+      post2 = Post.create(title: 'Second Post', text: 'Dolor sit amet', author_id: user.id)
 
-    it "assigns the user and posts" do
-      get user_posts_path(user_id: user.id)
+      expect(Post.count).to eq(2)
+
+      get :index, params: { user_id: user.id }
 
       expect(assigns(:user)).to eq(user)
-      expect(assigns(:posts)).to match_array(posts)
+      expect(assigns(:posts)).to contain_exactly(post1, post2)
     end
 
-    it "renders the index template" do
-      get user_posts_path(user_id: user.id)
+    it 'renders the index template' do
+      user = User.create(name: 'John Doe')
+
+      get :index, params: { user_id: user.id }
 
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET show" do
-    let!(:post) { Post.create(title: "Test Post") }
+  describe 'GET #show' do
+    it 'assigns the post' do
+      author = User.create!(name: 'John Doe')
+      post = Post.create!(title: 'First Post', text: 'Lorem ipsum', author: author)
 
-    it "assigns the post" do
-      get user_post_path(user_id: post.author_id, id: post.id)
+      get :show, params: { id: post.id }
 
       expect(assigns(:post)).to eq(post)
     end
 
-    it "renders the show template" do
-      get user_post_path(user_id: post.author_id, id: post.id)
+    it 'renders the show template' do
+      author = User.create!(name: 'John Doe')
+      post = Post.create!(title: 'First Post', text: 'Lorem ipsum', author: author)
+
+      get :show, params: { id: post.id }
 
       expect(response).to render_template(:show)
     end
