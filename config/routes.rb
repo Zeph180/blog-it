cc20faaa3565b58bdcda210930b4d883d4cb117c
Zeph180 <zephrichards1@gmail.com>
2023-05-16 13:27:47 +0300

    Change post show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
   get 'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'posts/:id', to: 'posts#show', as: 'user_post'
+  
 end
