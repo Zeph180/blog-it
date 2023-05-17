49b5bfa39a95731c59831e9eab54f10799abca38
Zeph180 <zephrichards1@gmail.com>
2023-05-17 22:19:48 +0300

    Enhance controllers to fit requirements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,14 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id])
+    @comments = Comment.where(post_id: params[:post_id])
+    @likes = Like.where(post_id: params[:post_id])
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find_by(id: @post.author_id)
+    @comments = Comment.where(post_id: params[:post_id])
+    @likes = Like.where(post_id: params[:post_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @posts = Post.where(author_id: @users.pluck(:id))
   end
 
   def show
     @user = User.find_by(id: params[:id])
-    @posts = Post.where(user_id: params[:id]).order(created_at: :desc).limit(3)
+    @posts = Post.where(author_id: params[:id]).order(created_at: :desc).limit(3)
+    @comments = Comment.where(post_id: @posts.pluck(:id))
+    @likes = Like.where(post_id: @posts.pluck(:id))
   end
 end
